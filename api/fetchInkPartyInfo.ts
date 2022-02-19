ddf305918254000902f3c86e1d93aa5483c9cef3
fomoFi.icu <joneclub1990@gmail.com>
2022-02-20 00:23:34 +0800

    party 接口 限制每页50条

--- api/fetchInkPartyInfo.ts
+++ api/fetchInkPartyInfo.ts
@@ -6,14 +6,28 @@ import { accurateAdd, accurateDiv } from "../utils";
 const CONNECTION_STRING =
   "mongodb+srv://mr7s:utjfSY8YE56WLNv@cluster0.uncsh.mongodb.net/multichain?retryWrites=true&w=majority";
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
-  async function requestGet() {
+  async function requestGet(startAfter: number | null) {
     return new Promise((resolve, reject) => {
+      const queryMsg = {
+        parties_with_deposits: {
+          start_after: startAfter,
+          limit: 50,
+          is_ascending: true,
+        },
+      };
+      const encodeQueryMsg = JSON.stringify(queryMsg);
+      const requestParams = {
+        params: {
+          query_msg: encodeQueryMsg,
+        },
+      };
       axios
         .get(
-          "https://lcd.terra.dev/wasm/contracts/terra1p4y54kfdn9uhvh62rjvgz3sydceuw9s6c65aef/store?query_msg=%7B%22parties_with_deposits%22:%7B%22start_after%22:null,%22limit%22:10000,%22is_ascending%22:true%7D%7D"
+          "https://lcd.terra.dev/wasm/contracts/terra1p4y54kfdn9uhvh62rjvgz3sydceuw9s6c65aef/store",
+          requestParams
         )
         .then(function (response) {
-          // console.log(response.data);
+          console.log(response.data);
           resolve(response.data);
         })
         .catch(function (error) {
@@ -22,28 +36,39 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
         });
     });
   }
-  try {
-    const resultData = await requestGet();
-    const client = await MongoClient.connect(CONNECTION_STRING, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    const db = await client.db("inker-data");
-    let partyTotalAmount = 0;
-    let partyPlayersCount = 0;
-    resultData.result.parties.forEach((v) => {
-      partyTotalAmount = accurateAdd(partyTotalAmount, v.total_deposit);
-      partyPlayersCount += v.current_member;
-    });
-    const saveData = {
-      partyCount: resultData.result.parties.length,
-      create: Date.now(),
-      partyTotalAmount: accurateDiv(partyTotalAmount, 1e6),
-      partyPlayersCount,
-    };
-    db.collection("party_info").insertOne(saveData);
-    res.status(200).json({ result: "ok" });
-  } catch (e) {
-    console.log(e);
-  }
+  let parties = [];
+  const fetchData = async (startAfter: number | null) => {
+    try {
+      const partyRes = await requestGet(startAfter);
+      const tempParties = partyRes?.result?.parties;
+      if (tempParties.length <= 0) {
+        const client = await MongoClient.connect(CONNECTION_STRING, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
+        const db = await client.db("inker-data");
+        let partyTotalAmount = 0;
+        let partyPlayersCount = 0;
+        parties.forEach((v) => {
+          partyTotalAmount = accurateAdd(partyTotalAmount, v.total_deposit);
+          partyPlayersCount += v.current_member;
+        });
+        const saveData = {
+          partyCount: parties.length,
+          create: Date.now(),
+          partyTotalAmount: accurateDiv(partyTotalAmount, 1e6),
+          partyPlayersCount,
+        };
+        db.collection("party_info").insertOne(saveData);
+        res.status(200).json({ result: "ok" });
+        return;
+      }
+      parties = [...parties, ...tempParties];
+      fetchData(tempParties[tempParties.length - 1].info.id);
+    } catch (e) {
+      /* eslint-disable no-console */
+      console.log("dashboard err", e);
+    }
+  };
+  fetchData(null);
 };
